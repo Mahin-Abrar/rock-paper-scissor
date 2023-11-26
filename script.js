ba3ddb2286cf28efe7fd9ea76bb05590993ac53b
Mahin-Abrar <mahinabrar25@gmail.com>
2023-11-26 13:59:05 +0600

    comments added and certain css files discarded

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {    ///variable declaration
 let userInput = document.querySelectorAll(".userButton");
 let playerChoice = document.getElementById("playerChoice");
 let output= $(".result");
@@ -11,6 +11,8 @@ let moves=10;
 let displayP=$(".playerC");
 let displayC=$(".pcC");
 
+/// function to trigger the random selection of the pc and count the moves
+ 
 userInput.forEach(function (button) {
     button.addEventListener("click", function () {
       playerChoice = button.innerHTML;
@@ -31,11 +33,15 @@ userInput.forEach(function (button) {
       movesRemain.text(moves);
     });
   });
+
+  //choices for the pc
   let choicesPc = {
     1: 'ROCK',
     2: 'PAPER',
     3: 'SCISSOR',
   };
+
+  //function for the output  and iteration of the score
   function results(){
     if(playerChoice==choicesPc[pcChoice]){
     output.html("<hr>"+"Draw");
@@ -68,6 +74,9 @@ userInput.forEach(function (button) {
 
   
 }); 
+
+
+//reload button functionality
 
 let reloadBtn = $(".reload");
 reloadBtn.on("click", function () {
